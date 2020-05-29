f2cecdeac85a476cd24d70aec8e625e4b302ae43
vmcgidev <vmcgidev@cgi-vm>
2020-05-29 21:33:51 +0000

    Add project files.

---
+++ Employee.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Employee1", "Employee2" };
+        }
+    }
+}
